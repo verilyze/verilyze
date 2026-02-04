47d684c082125e0c8eeb086a831f425858c1a28a
Travis Post <post.travis@gmail.com>
2026-02-04 11:57:41 -0800

    feat: add spd-plugin-macro for plugin registration
    - Introduce spd-plugin-macro to facilitate plugin registration in the
      spd binary, enhancing the registration process and avoiding circular
      dependencies.
    - Update registry.rs to utilize the new spd_register! macro for default
      plugin registration, streamlining the code and improving
      maintainability.
    
    Signed-off-by: Travis Post <post.travis@gmail.com>

--- spd-plugin-macro/src/lib.rs
+++ spd-plugin-macro/src/lib.rs
@@ -1,30 +1,42 @@
-//! Helper macro used by plug‑in crates to register themselves.
+//! Helper macro used by the binary to register default plugins.
+//!
+//! Expands `spd_register!(PluginKind, ConcreteType)` to a call to
+//! `crate::registry::register(Plugin::PluginKind(Box::new(ConcreteType::new())))`.
+//! Used in the spd binary so plugin registration is driven by the binary (avoids
+//! circular dependency with a separate spd-registry crate).
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Ident};
+use syn::parse::{Parse, ParseStream};
+use syn::{Ident, Token};
+
+struct RegisterCall {
+    variant: Ident,
+    concrete_type: Ident,
+}
+
+impl Parse for RegisterCall {
+    fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
+        let variant: Ident = input.parse()?;
+        input.parse::<Token![,]>()?;
+        let concrete_type: Ident = input.parse()?;
+        Ok(RegisterCall {
+            variant,
+            concrete_type,
+        })
+    }
+}
 
 #[proc_macro]
 pub fn spd_register(input: TokenStream) -> TokenStream {
-    // Expected syntax: spd_register!(MyFinder, MyParser, MyProvider);
-    let ids = parse_macro_input!(input as syn::punctuated::Punctuated<Ident, syn::Token![,]>);
+    let RegisterCall {
+        variant,
+        concrete_type,
+    } = syn::parse_macro_input!(input as RegisterCall);
 
-    let mut pushers = Vec::new();
-    for id in ids.iter() {
-        let var = syn::Ident::new(
-            &format!("REGISTRY_{}", id.to_string().to_uppercase()),
-            id.span(),
-        );
-        pushers.push(quote! {
-            lazy_static::lazy_static! {
-                static ref #var: () = {
-                    spd::registry::register(Box::new(#id));
-                };
-            }
-        });
-    }
+    let expanded = quote! {
+        crate::registry::register(crate::registry::Plugin::#variant(Box::new(#concrete_type::new())));
+    };
 
-    TokenStream::from(quote! {
-        #(#pushers)*
-    })
+    TokenStream::from(expanded)
 }
